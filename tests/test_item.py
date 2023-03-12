14b24da7a5d7a1f83d51e840dea58c3e4cf98bbd
WRXer <king_311@mail.ru>
2023-03-12 12:28:56 +0400

    Написан: Тест функции name Тест статического метода

--- tests/test_item.py
+++ tests/test_item.py
@@ -37,3 +37,32 @@ def test_apply_discount(item):
     Item.pay_rate = 0.5
     item.apply_discount()
     assert item.price == 4000
+
+def test_name_1(item):
+    """
+    Тест функции name
+    """
+    item.name = "test"
+    assert item.name == "test"
+
+#def test_name_2(item):
+    #"""
+    #Тест функции name
+    #"""
+    #item.name = "test_for_ten_symbols"
+    #assert item.name != "test_for_ten_symbols"
+
+def test_name_3(item):
+    """
+    Тест функции name
+    """
+    item.name = "test_for_ten_symbols"
+    with pytest.raises(ValueError, match="Длина наименования товара превышает 10 символов."):
+        item.name()
+
+def test_string_to_number(item):
+    """
+    Тест статического метода
+    """
+    assert item.string_to_number("5.0") == 5
+
